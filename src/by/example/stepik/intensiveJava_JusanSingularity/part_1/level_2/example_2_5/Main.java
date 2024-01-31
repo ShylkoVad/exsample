74a8835cbef566e20555e48a4ab2c3d9245ff961
ShylkoVad <shylkovad@gmail.com>
2024-01-31 09:11:33 +0300

    31/01/2024 9:11

--- src/by/example/stepik/intensiveJava_JusanSingularity/part_1/level_2/example_2_5/Main.java
+++ src/by/example/stepik/intensiveJava_JusanSingularity/part_1/level_2/example_2_5/Main.java
@@ -3,6 +3,29 @@ package by.example.stepik.intensiveJava_JusanSingularity.part_1.level_2.example_
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println(sum1n(51261));
+        System.out.println(countLeapYear(100));
+    }
+
+    public static int sum1n(int a) {
+        int sum = 0;
+        if (a >= 1 && a <= 65535) {
+            if (a % 2 == 0) {
+                sum = a / 2 * (a + 1);
+            } else {
+                sum = (a + 1) / 2 * a;
+            }
+        }
+        return sum;
+    }
+
+    public static int countLeapYear(int n) {
+        int year = 0;
+        if (n > 0) {
+            year = (n / 4) - (n / 100) + (n / 400);
+        }
+
+        return year;
     }
 
 
