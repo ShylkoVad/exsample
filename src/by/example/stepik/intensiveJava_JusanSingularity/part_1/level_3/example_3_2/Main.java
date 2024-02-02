00a0e7a7c2dadcc1629034ee11f7f9a8f8f083c3
Vadzim <shylkovad@gmail.com>
2024-02-02 19:58:56 +0300

    02/02/2024 19:58

--- src/by/example/stepik/intensiveJava_JusanSingularity/part_1/level_3/example_3_2/Main.java
+++ src/by/example/stepik/intensiveJava_JusanSingularity/part_1/level_3/example_3_2/Main.java
@@ -235,17 +235,18 @@ public class Main {
 
     public static void sort(int[] array) {
 
-        int arrayI = array[0];
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] <= arrayI) {
-                int temp;
-                temp = array[i];
-                array[i] = array[i];
+        int temp;
 
-
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
             }
         }
-
+        System.out.println(Arrays.toString(array));
     }
 
 
