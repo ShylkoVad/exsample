02a6818a9b4e99f13a9aba93aa80de97e2388857
ShylkoVad <shylkovad@gmail.com>
2024-03-05 15:47:46 +0300

    05/03/2024 15:34

--- src/by/example/yandex/algorithms5_0/lecture1/PaintingTrees.java
+++ src/by/example/yandex/algorithms5_0/lecture1/PaintingTrees.java
@@ -1,6 +1,7 @@
 package by.example.yandex.algorithms5_0.lecture1;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PaintingTrees {
@@ -10,19 +11,15 @@ public class PaintingTrees {
         int V = sc.nextInt(); // на сколько деревьев Вася может удалиться
         int Q = sc.nextInt(); // ведро для Маши установлено возле дерева Q
         int M = sc.nextInt(); // на сколько деревьев Маша может удалиться
-
         ArrayList<Integer> pv = new ArrayList<>();
         for (int i = 0, j = 0; i < (V * 2 + 1); i++)
             pv.add(P + V - (j++));
 
         for (int i = 0, j = 0; i < (M * 2 + 1); i++) {
             pv.add(Q + M - (j++));
-            for (int a = 0; a < pv.size() - 1; a++) {
-                if (pv.get(pv.size() - 1).equals(pv.get(a))) {
-                    pv.remove(a);
-                }
-            }
         }
-        System.out.println(pv.size());
+        List<Integer> listWithoutDuplicates =
+                pv.stream().distinct().toList();
+        System.out.println(listWithoutDuplicates.size());
     }
 }
