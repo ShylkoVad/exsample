51bb1e6bb065b9fea69021ad6061b3a8e0ca16b1
ShylkoVad <shylkovad@gmail.com>
2024-03-05 15:34:43 +0300

    05/03/2024 15:34

--- src/by/example/yandex/algorithms5_0/lecture1/PaintingTrees.java
+++ src/by/example/yandex/algorithms5_0/lecture1/PaintingTrees.java
@@ -1,10 +1,9 @@
 package by.example.yandex.algorithms5_0.lecture1;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-public class paintingTrees {
+public class PaintingTrees {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int P = sc.nextInt(); // ведро для Васи установлено возле дерева P
@@ -12,20 +11,15 @@ public class paintingTrees {
         int Q = sc.nextInt(); // ведро для Маши установлено возле дерева Q
         int M = sc.nextInt(); // на сколько деревьев Маша может удалиться
 
-        List<Integer> pv = new ArrayList<>();
-
-        for (int i = 0, j = 0; i < (V * 2 + 1); i++) {
+        ArrayList<Integer> pv = new ArrayList<>();
+        for (int i = 0, j = 0; i < (V * 2 + 1); i++)
             pv.add(P + V - (j++));
-        }
 
         for (int i = 0, j = 0; i < (M * 2 + 1); i++) {
             pv.add(Q + M - (j++));
-
-        }
-        for (int i = 0; i < pv.size(); i++) {
-            for (int j = i + 1; j < pv.size(); j++) {
-                if (pv.get(i).equals(pv.get(j))) {
-                    pv.remove(j);
+            for (int a = 0; a < pv.size() - 1; a++) {
+                if (pv.get(pv.size() - 1).equals(pv.get(a))) {
+                    pv.remove(a);
                 }
             }
         }
